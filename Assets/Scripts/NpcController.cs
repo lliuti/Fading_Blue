ff138960842ef47f4cd18e68b85f1cadb2bf704f
Lucas Liuti <dev.lliuti@gmail.com>
2024-03-05 13:46:03 -0300

    feat - NPC dialogue system

---
+++ Assets/Scripts/NpcController.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NpcController : MonoBehaviour
+{
+    [SerializeField] private GameObject dialogueCanvas;
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private string[] dialogue;
+    [SerializeField] private float typeSpeed;
+    private int dialogueIndex;
+    private bool playerOnRange = false;
+    private bool inDialogue = false;
+
+    void Start()
+    {
+        dialogueText.text = "";
+    }
+
+    void Update()
+    {
+    }
+
+    void OnInteract()
+    {
+        if (!playerOnRange) return;
+
+        if (inDialogue) {
+            StopAllCoroutines();
+            NextLine();
+            return;
+        }
+
+        if (dialogueCanvas.activeInHierarchy) {
+            ClearDialogue();
+            return;
+        } 
+
+        inDialogue = true;
+        dialogueCanvas.SetActive(true);
+        StartCoroutine(Typing());
+    }
+
+    void NextLine()
+    {
+        if (dialogueIndex < dialogue.Length - 1) {
+            dialogueIndex++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        } else {
+            ClearDialogue();
+        };
+
+    }
+
+    void ClearDialogue()
+    {
+        dialogueText.text = "";
+        dialogueIndex = 0;
+        dialogueCanvas.SetActive(false);
+        inDialogue = false;
+    }
+
+    IEnumerator Typing()
+    {
+        foreach (char letter in dialogue[dialogueIndex].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(typeSpeed);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (!other.CompareTag("Player")) return;
+        playerOnRange = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other) 
+    {
+        if (!other.CompareTag("Player")) return;
+        playerOnRange = false;
+        ClearDialogue();
+    }
+
+}
