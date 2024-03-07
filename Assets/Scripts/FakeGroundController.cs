e68b7bc18e45da024a3fc70c68a15dca4eff25c0
Lucas Liuti <dev.lliuti@gmail.com>
2024-03-07 19:14:07 -0300

    feat - adding fake ground

---
+++ Assets/Scripts/FakeGroundController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeGroundController : MonoBehaviour
+{
+    [SerializeField] private GameObject crystal;
+    private Animator animator;
+    private bool hasEntered = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();        
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (!other.CompareTag("Player") || hasEntered) return;
+        animator.SetTrigger("Disappear");
+        crystal.SetActive(true);
+        hasEntered = true;
+    }
+}
