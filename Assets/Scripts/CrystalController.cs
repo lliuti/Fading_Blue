27c28daf760869b1d1d2cc85d8f3b7882d22f842
Lucas Liuti <dev.lliuti@gmail.com>
2024-03-07 20:05:14 -0300

    refactor - checks if it is starting scene to destroy the lock or open the gate

--- Assets/Scripts/CrystalController.cs
+++ Assets/Scripts/CrystalController.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrystalController : MonoBehaviour
 {
-    [SerializeField] private GameObject doorLock; 
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
+
         other.GetComponent<PlayerController>().collectedCrystal = true;
-        Destroy(doorLock);
         Destroy(gameObject);
+
+        Scene scene = SceneManager.GetActiveScene();
+
+        if (scene.buildIndex == 0) {
+            GameObject doorLock = GameObject.Find("Lock");
+            Destroy(doorLock);
+        } else {
+            GameObject gate = GameObject.Find("Gate");
+            gate.GetComponent<Animator>().SetTrigger("Open");
+        }
+
     }
 }
