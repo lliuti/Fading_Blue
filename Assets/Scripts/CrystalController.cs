97a3f306e168526315d7772722f0002e69b0c551
Lucas Liuti <dev.lliuti@gmail.com>
2024-03-07 11:05:31 -0300

    feat - adding lock to the main house

--- Assets/Scripts/CrystalController.cs
+++ Assets/Scripts/CrystalController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CrystalController : MonoBehaviour
 {
+    [SerializeField] private GameObject doorLock; 
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
         other.GetComponent<PlayerController>().collectedFirstCrystal = true;
+        Destroy(doorLock);
         Destroy(gameObject);
     }
 }
