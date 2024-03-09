8d452f4bebcf5a163db6fecbc4d009aada1cd964
Lucas Liuti <dev.lliuti@gmail.com>
2024-03-09 14:47:11 -0300

    feat - adding gate opening sound fx

---
+++ Assets/Scripts/GateController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateController : MonoBehaviour
+{
+    public static GateController instance;
+    [SerializeField] private AudioClip gateOpeningClip;
+    private Animator animator;
+
+    void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OpenGate()
+    {
+        animator.SetTrigger("Open");
+        SoundFXManager.instance.PlaySoundFXClip(gateOpeningClip, transform, 0.5f);
+    }
+
+}
