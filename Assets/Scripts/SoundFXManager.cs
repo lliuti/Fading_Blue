e7f59bfa97fb92fdf4546acb13606dcc56dc9f44
Lucas Liuti <dev.lliuti@gmail.com>
2024-03-09 14:48:29 -0300

    fix - adding destroy to singleton

--- Assets/Scripts/SoundFXManager.cs
+++ Assets/Scripts/SoundFXManager.cs
@@ -9,7 +9,11 @@ public class SoundFXManager : MonoBehaviour
 
     void Awake()
     {
-        if (instance == null) instance = this;
+        if (instance == null) {
+            instance = this;
+        } else {    
+            Destroy(gameObject);
+        };
     }
 
     public void PlaySoundFXClip(AudioClip clip, Transform spawnTransform, float volume)
