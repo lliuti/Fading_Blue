d787d4a280bd1290e80a552221c892ce5e52cd76
Lucas Liuti <dev.lliuti@gmail.com>
2024-03-10 14:22:06 -0300

    feat - adding lock breaking animation

---
+++ Assets/Scripts/LockController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockController : MonoBehaviour
+{
+    [SerializeField] private GameObject interactionIndicator;
+    private Animator animator;
+    private bool destroyed = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+        if (!other.GetComponent<PlayerController>().collectedCrystal) return;
+        
+        interactionIndicator.SetActive(true);
+
+        if (destroyed) return;
+        
+        destroyed = true;
+        animator.SetTrigger("Break");
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+        interactionIndicator.SetActive(false);
+    }
+}
