ee8dc024842366657058de99c66c1aa9379f7b44
Lucas Liuti <dev.lliuti@gmail.com>
2024-03-11 01:54:50 -0300

    chore - removing unused methods

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -14,16 +14,4 @@ public class MusicManager : MonoBehaviour
             Destroy(gameObject);
         };
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
