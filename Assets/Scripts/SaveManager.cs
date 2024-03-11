eae8061e4ac749d9ce980c077108956b84b4ad4f
Lucas Liuti <dev.lliuti@gmail.com>
2024-03-11 01:54:09 -0300

    feat - adding save manager

---
+++ Assets/Scripts/SaveManager.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager instance;
+    public bool existingGame = false;
+    private string path;
+
+    void Awake()
+    {
+        if (instance == null) {
+            instance = this;
+        } else {
+            Destroy(gameObject);
+        };
+    }
+
+    void Start()
+    {
+        path = Application.persistentDataPath + "/acerola_0.ls";
+        if (SceneManager.GetActiveScene().buildIndex > 0) Save();
+    }
+
+    public bool PreviousGame()
+    {
+        return File.Exists(path);
+    }
+
+    public void Save()
+    {
+        Scene level = SceneManager.GetActiveScene();
+        int index = level.buildIndex;
+
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream stream = new FileStream(path, FileMode.Create);
+        formatter.Serialize(stream, index);
+        stream.Close();
+    }
+
+    public void Load()
+    {
+        if (!PreviousGame()) return;
+
+        BinaryFormatter binaryFormatter = new BinaryFormatter(); 
+        FileStream stream = new FileStream(path, FileMode.Open);
+        int loadedIndex = (int)binaryFormatter.Deserialize(stream);
+        stream.Close();
+
+        if (!(loadedIndex == SceneManager.GetActiveScene().buildIndex)) {
+            SceneManager.LoadScene(loadedIndex, LoadSceneMode.Single);
+        }
+    }
+}
