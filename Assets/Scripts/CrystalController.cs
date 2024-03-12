f68f94e1769f2ea281c1d12a9c158d15a34b6a7d
Lucas Liuti <dev.lliuti@gmail.com>
2024-03-12 18:47:02 -0300

    feat - adding blue crystal

--- Assets/Scripts/CrystalController.cs
+++ Assets/Scripts/CrystalController.cs
@@ -1,22 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
 
 public class CrystalController : MonoBehaviour
 {
 
     [SerializeField] private AudioClip collectingClip;
+    [SerializeField] private bool blueCrystal = false;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
-        other.GetComponent<PlayerController>().collectedCrystal = true;
+        PlayerController player = other.GetComponent<PlayerController>();
 
-        SoundFXManager.instance.PlaySoundFXClip(collectingClip, transform, 1f);
         Destroy(gameObject);
 
-        Scene scene = SceneManager.GetActiveScene();
-        if (scene.name != "Scene1") GateController.instance.OpenGate();
+        if (SceneManager.GetActiveScene().name != "Scene1" && !blueCrystal) GateController.instance.OpenGate();
+
+        CollectedCrystal(player);
+    }
+
+    void CollectedCrystal(PlayerController player)
+    {
+        player.collectedCrystal = true;
+        player.crystalParticles.Play();
+    
+        SoundFXManager.instance.PlaySoundFXClip(collectingClip, transform, 1f);
+
+        Light2D light = player.transform.Find("Light 2D").GetComponent<Light2D>();
+        if (blueCrystal) {
+            light.color = new Color(0f/255, 154f/255, 255f/255, 255f/255);
+            light.intensity = 2f;
+            light.pointLightOuterRadius = 5f;
+
+            Light2D globalLight = GameObject.Find("Global Light 2D").GetComponent<Light2D>();
+            globalLight.intensity = 1;
+            globalLight.color = new Color(84f/255, 224f/255, 224f/255, 255f/255);
+        } else {
+            light.color = new Color(255f/255, 1f/255, 1f/255, 255f/255);
+        }
     }
 }
